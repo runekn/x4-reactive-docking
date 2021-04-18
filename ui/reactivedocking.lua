4546fbe12b142bf795ce6076bbadab325a98a7ea
runekn <rune@beast.dk>
2021-04-18 11:10:59 +0200

    Disabled reactive option while no captain. Updated version and date in content.xml

--- ui/reactivedocking.lua
+++ ui/reactivedocking.lua
@@ -13,6 +13,11 @@ local config = {
 		[2] = { id = "launched",		text = ReadText(1001, 8629),	icon = "",	displayremoveoption = false },
 		[3] = { id = "reactive",		text = ReadText(181114415, 1),	icon = "",	displayremoveoption = false }
 	},
+	subordinatedockingoptions_nopilot = {
+		[1] = { id = "docked",			text = ReadText(1001, 8630),	icon = "",	displayremoveoption = false },
+		[2] = { id = "launched",		text = ReadText(1001, 8629),	icon = "",	displayremoveoption = false },
+		[3] = { id = "reactive",		text = ReadText(181114415, 1),	icon = "",	displayremoveoption = false, active = false, mouseovertext = ReadText(181114415, 2) }
+	},
 	mapRowHeight = Helper.standardTextHeight
 }
 
@@ -38,6 +43,9 @@ end
 
 local function getReactiveDocking(inputobject, i)
 	local pilotentityid = GetControlEntity(inputobject)
+	if pilotentityid == nil then
+		return false
+	end
 	local reactiveList = GetNPCBlackboard(pilotentityid, "$DockingReactive")
 	if reactiveList == nil or reactiveList[i] == nil then
 		return false
@@ -70,37 +78,43 @@ local function setDockingOptions(inputobject, i, newdockingoption)
 	C.SetSubordinateGroupDockAtCommander(inputobject, i, docked)
 	setReactiveDocking(inputobject, i, reactive)
 end
+
+local function getDockingOptionsList(inputobject)
+	local pilotentityid = GetControlEntity(inputobject)
+	if pilotentityid == nil then
+		return config.subordinatedockingoptions_nopilot
+	else
+		return config.subordinatedockingoptions
+	end
+end
  
 function do_menu.addReactiveDockingMapMenu(row, inputobject, i, mode, active, mouseovertext)
 	local menu = map_menu
 
 	-- Just create the vanilla button if its not a ship or a carrier
-	if mode ~= "ship" or GetComponentData(inputobject, "shiptype") == "carrier" then
+	if mode ~= "ship" or inputobject == nil or GetComponentData(inputobject, "shiptype") == "carrier" then
 		row[3]:setColSpan(11):createButton({ active = active, mouseOverText = mouseovertext, height = config.mapRowHeight }):setText(function () return C.ShouldSubordinateGroupDockAtCommander(inputobject, i) and ReadText(1001, 8630) or ReadText(1001, 8629) end, { halign = "center" })
 		row[3].handlers.onClick = function () return C.SetSubordinateGroupDockAtCommander(inputobject, i, not C.ShouldSubordinateGroupDockAtCommander(inputobject, i)) end
 	-- Otherwise create a dropdown with the extra option
 	else
-		row[3]:setColSpan(11):createDropDown(config.subordinatedockingoptions, { active = active, mouseOverText = mouseovertext, height = config.mapRowHeight, startOption = function () return getDockingStartingOrder(inputobject, i) end })
+		row[3]:setColSpan(11):createDropDown(getDockingOptionsList(inputobject), { active = active, mouseOverText = mouseovertext, height = config.mapRowHeight, startOption = function () return getDockingStartingOrder(inputobject, i) end })
 		row[3].handlers.onDropDownActivated = function () menu.noupdate = true end
 		row[3].handlers.onDropDownConfirmed = function (_, newdockingoption) setDockingOptions(inputobject, i, newdockingoption); menu.noupdate = false end
 	end
 	return true
 end
 
-
 function do_menu.addReactiveDockingDockMenu(row, inputobject, i, active, mouseovertext)
 	local menu = dock_menu
 
-	local shiptype = GetComponentData(menu.currentplayership, "shiptype")
-	local iscarrier = shiptype == "carrier"
 	-- Just create the vanilla button if its a carrier
-	if iscarrier then
+	if inputobject == nil or GetComponentData(inputobject, "shiptype") == "carrier" then
 		row[7]:setColSpan(5):createButton({ active = active, mouseOverText = mouseovertext }):setText(function () return C.ShouldSubordinateGroupDockAtCommander(menu.currentplayership, i) and ReadText(1001, 8630) or ReadText(1001, 8629) end, { halign = "center" })
 		row[7].handlers.onClick = function () return C.SetSubordinateGroupDockAtCommander(menu.currentplayership, i, not C.ShouldSubordinateGroupDockAtCommander(menu.currentplayership, i)) end
 	-- Otherwise create a dropdown with the extra option
 	else
-		row[7]:setColSpan(5):createDropDown(config.subordinatedockingoptions, { active = active, mouseOverText = mouseovertext, startOption = function () return getDockingStartingOrder(menu.currentplayership, i) end })
-		row[7].handlers.onDropDownConfirmed = function (_, newdockingoption) setDockingOptions(menu.currentplayership, i, newdockingoption) end
+		row[7]:setColSpan(5):createDropDown(getDockingOptionsList(inputobject), { active = active, mouseOverText = mouseovertext, startOption = function () return getDockingStartingOrder(inputobject, i) end })
+		row[7].handlers.onDropDownConfirmed = function (_, newdockingoption) setDockingOptions(inputobject, i, newdockingoption) end
 	end
 	return true
 end 
